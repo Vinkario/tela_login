502aa7e20135d5efb957e7961d151ed547b3a8c4
Rogerio Bueno <gadblake66@gmail.com>
2022-06-01 11:58:35 -0300

    input criado e estilizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+     <div className="container-login">
+       <div className="wrap-login">
+         <form className="login-form">
+           <span className="login-form-title">Bem Vindo</span>
+
+           <div className="wrap-input">
+             <input className="input" type="email"  placeholder="Email"></input>
+             <span className="focus-input" data-placeholder="Email"></span>
+           </div>
+
+            <div className="wrap-input">
+              <input className="input" type="password" placeholder="Senha"></input>
+              <span className="focus-input" data-placeholder="Senha"></span>
+            </div>
+
+         </form>
+       </div>
+     </div>
+   </div>
   );
 }
 
